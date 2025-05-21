19cb849dea022b9263431704948ffa254af9292d
nathan <nathankwok95@gmail.com>
2025-05-21 19:43:28 +1000

    Created initial page

--- task-frontend/src/App.tsx
+++ task-frontend/src/App.tsx
@@ -1,44 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SplashScreen from "./pages/SplashScreen";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col items-center justify-center text-white p-8">
-      <div className="flex space-x-8 mb-6">
-        <a href="https://vite.dev" target="_blank">
-          <img
-            src={viteLogo}
-            className="w-16 h-16 hover:scale-110 transition-transform duration-300"
-            alt="Vite logo"
-          />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="w-16 h-16 hover:scale-110 transition-transform duration-300"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1 className="text-4xl font-bold mb-4">Vite + React + Tailwind</h1>
-      <div className="card bg-white text-black p-6 rounded-xl shadow-lg">
-        <button
-          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded mb-4"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="mt-6 text-sm italic">Click on the logos to learn more</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<SplashScreen />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ task-frontend/src/pages/SplashScreen.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import splashImage from "../assets/splash.png";
+
+const SplashScreen = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen px-6 text-center">
+      <img src={splashImage} alt="Task Manager" className="w-64 mb-8" />
+      <h1 className="text-3xl font-bold mb-8">
+        STAY ON TOP OF TASKS, <br />
+        STRESS-FREE
+      </h1>
+      <button className="[background-color:#ff50be] hover:[background-color:#e647aa] font-bold py-3 px-12 rounded-full transition-colors cursor-pointer w-96">
+        Get started
+      </button>
+    </div>
+  );
+};
+
+export default SplashScreen;
